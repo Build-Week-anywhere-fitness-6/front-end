420ecf8b5879754a06f82888de9fd2872b516791
prashishb <prashb.web@gmail.com>
2021-10-18 15:30:08 -0400

    created login and register component and added basic CSS for now

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <form className='form-container'>
+      <h1>Login</h1>
+      <div className='form-field'>
+        {/* USERNAME */}
+        {/* PASSWORD */}
+        {/* LOGIN BUTTON */}
+      </div>
+    </form>
+  );
+}
