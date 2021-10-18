5b1d3230802b2dfaeee32add39f834673a51bdeb
Nathan <nathan.forman@gmail.com>
2021-10-18 12:48:30 -0700

    Added PrivateRoute component

---
+++ src/components/Register.js
@@ -0,0 +1,14 @@
+export default function Register() {
+  return (
+    <form className='form-container'>
+      <h1>Sign Up</h1>
+      <div className='form-field'>
+        {/* EMAIL */}
+        {/* USERNAME */}
+        {/* PASSWORD */}
+        {/* ROLE */}
+        {/* SIGNUP BUTTON */}
+      </div>
+    </form>
+  );
+}
