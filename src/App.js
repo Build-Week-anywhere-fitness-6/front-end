d3699e09e4a3b05a64acf1b61f8b55c673f8ca6f
Nathan <nathan.forman@gmail.com>
2021-10-19 10:57:34 -0700

    Added handleChange and handleSubmit functions to Register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PrivateRoute from './components/PrivateRoute';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
